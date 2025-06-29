221b738d0edd783e2b257c5389a67532b6e1ec57
Mohammad Mahdi Takfallah <mmtmmt945@gmail.com>
2025-06-29 20:10:31 +0330

    .
    .

--- AirLine/urls.py
+++ AirLine/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts'),),
+    path('', home.as_view(), name='home'),
 ]
