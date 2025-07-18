6f8dc7e13fa6b152892c8b1628f2c2e7b6b0c52b
Mohammad Mahdi Takfallah <mmtmmt945@gmail.com>
2025-07-18 12:18:01 +0330

    half way to the flight detail page
    half way to the flight detail page and we need a very good frontend for it.

--- flights/urls.py
+++ flights/urls.py
@@ -1,5 +1,11 @@
 # Django packages
 from django.urls import path
 
+# in app models
+from . import views
+
 app_name = 'flights'
-urlpatterns = []
+
+urlpatterns = [
+    path('flight_detail/<int:flight_id>/', views.flight_detail.as_view(), name='flight_detail'),
+]
--- flights/views.py
+++ flights/views.py
@@ -7,7 +7,24 @@ from django.contrib import messages
 from flights.models import Flights, Tickets
 
 
+# view for showing the home page
 class home(View):
+    """
+    this is a view to show the objects of the home page
+    """
+
     def get(self, request):
         all_flights = Flights.objects.all()
         return render(request, 'flights/home.html', {'all_flights': all_flights})
+
+
+# flight detail view to show the detail of the flight
+class flight_detail(View):
+    """
+    this is a view to show the flight details in a new page and book the Ticket
+    """
+    template_name = 'flights/flight_detail.html'
+
+    def get(self, request, flight_id):
+        flight = get_object_or_404(Flights, flight_id=flight_id)
+        return render(request, self.template_name, {'flight': flight})
